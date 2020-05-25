0ce3bedc751821d53854f5f493d55b7275efed49
A. Rzhanitsyn <risdeep@gmail.com>
2020-05-25 22:51:43 +0500

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 import App from "./App.jsx";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+let container = null;
+beforeEach(() => {
+  // подготавливаем DOM-элемент, куда будем рендерить
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // подчищаем после завершения
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("render header", () => {
+  act(() => {
+    render(<App />, container);
+  });
+
+  const linkElement = document.querySelector("header > h1");
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement.textContent).toBe("Test APP");
 });
